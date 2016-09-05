aedffc40c5125650ecbdb247b69e1f9f6a06edf9
nasbsil14 <nasbsil14@yahoo.co.jp>
2016-09-05 15:32:24 +0900

    up

--- goodsContext/src/test/scala/GoodsModelSpec.scala
+++ goodsContext/src/test/scala/GoodsModelSpec.scala
@@ -1,12 +1,11 @@
 import org.scalatest.FlatSpec
-import ec.goods.domain.models
-import ec.goods.domain.models.entities.Goods
+import ec.goods.domain.models.entities.{Goods, GoodsList}
+import ec.goods.domain.models.valueObjects.GoodsId
 
 class GoodsModelSpec extends FlatSpec {
   //商品リストが見れる
   it should "show goods list" in {
-    val x = Goods
-    x.
+    val x = GoodsList(List(new GoodsId("")))
   }
   //特定の商品の詳細情報が見れる
   it should "show goods detail" in {
---
+++ orderContext/src/test/scala/OrderModelSpec.scala
@@ -0,0 +1,8 @@
+import org.scalatest.FlatSpec
+
+class OrderModelSpec extends FlatSpec {
+  //特定の商品の詳細情報が見れる
+  it should "show order detail" in {
+
+  }
+}
